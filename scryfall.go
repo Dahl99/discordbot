37e191fb133093d301b1e8988045bc816694328b
dahl99 <daniedah@stud.ntnu.no>
2019-11-10 16:00:51 +0100

    Added some more comments and removed spaghett

--- scryfall.go
+++ scryfall.go
@@ -13,6 +13,9 @@ const scryfallBaseURL string = "https://api.scryfall.com/cards/"
 //Const containing string to be sent if scryfall API is unavailable
 const scryfallNotAvailable string = "Scryfall API not available at the moment. Please try again later."
 
+//Const containing string to be sent if decoding fails
+const decodingFailed string = "Something wrong happened when decoding data from Scryfall"
+
 //Struct to store info from first http.Get() and autocomplete search
 type autoResult struct {
 	Data []string `json:"data"`
@@ -30,8 +33,8 @@ type exactResult struct {
 
 //getCard() fetches a card based on which card name used in command
 func getCard(n []string) string {
-	var name string
-	name = replaceSpace(n, true)                      // Replaces the spaces with "_" to avoid url problems
+
+	name := replaceSpace(removeOrdMatter(n, 0))       // Replaces the spaces with "_" to avoid url problems
 	URL := scryfallBaseURL + "autocomplete?q=" + name // Sets url for autocomplete get request
 
 	res, err := http.Get(URL) // Fetching most probable card using scryfall autocomplete
@@ -43,12 +46,15 @@ func getCard(n []string) string {
 	//	Decoding results into autoresult struct object
 	var autoresult autoResult
 	err = json.NewDecoder(res.Body).Decode(&autoresult)
-	res.Body.Close()
+	if err != nil {
+		log.Println(err)
+		return decodingFailed
+	}
+	res.Body.Close() // Closing body to prevent resource leak
 
 	cardName := strings.Split(autoresult.Data[0], " ") // Splitting string on each space
-	name = replaceSpace(cardName, false)               // Replaceing space with "_" to avoid url problems
-
-	URL = scryfallBaseURL + "named?exact=" + name // Sets url for exact card get request
+	name = replaceSpace(cardName)                      // Replaceing space with "_" to avoid url problems
+	URL = scryfallBaseURL + "named?exact=" + name      // Sets url for exact card get request
 
 	res, err = http.Get(URL) // Fetching exact card
 	if err != nil {          // Checking for errors
@@ -59,26 +65,22 @@ func getCard(n []string) string {
 	// Decoding results into exactResult
 	var card exactResult
 	err = json.NewDecoder(res.Body).Decode(&card)
-	res.Body.Close()
+	if err != nil {
+		log.Println(err)
+		return decodingFailed
+	}
+	res.Body.Close() // Closing body to prevent resource leak
 
 	return card.Image.Png // Returning url to png version of card
 }
 
 //	This function replaces spaces in a string slice with "_"
-func replaceSpace(s []string, b bool) string {
+func replaceSpace(s []string) string {
 	var retString string //	String to be returned
-	var i int            // Used in for loop
-	var sliceLen int     // Used to determine how long slice must be to be replaced
 
-	if b { // if true loop will start at 1 and slice must be >= 3
-		i = 1
-		sliceLen = 3
-	} else { // if false loop will start at 0 and slice must be >= 2
-		i = 0
-		sliceLen = 2
-	}
+	i := 0
 
-	if len(s) >= sliceLen { // Checks if name is more than one word
+	if len(s) >= 2 { // Checks if name is more than one word
 		for i < len(s) { // Loops through slice and adds index
 			retString += s[i]
 
@@ -93,4 +95,9 @@ func replaceSpace(s []string, b bool) string {
 	}
 
 	return retString // Returns name with "_" instead of spaces or 1 word name
+}
+
+//	Removes an index from a string slice while keeping same order
+func removeOrdMatter(s []string, ind int) []string {
+	return append(s[:ind], s[ind+1:]...)
 }
