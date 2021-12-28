e7d5ee182b617bf4d28d278488caa5f5ccfe24e9
Daniel Dahl <danieldahl99@gmail.com>
2021-12-28 22:17:24 +0100

    Retry connection to database

--- src/database/database.go
+++ src/database/database.go
@@ -2,6 +2,7 @@ package database
 
 import (
 	"log"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -11,8 +12,14 @@ var DB *gorm.DB
 
 func Connect() {
 	var err error
+	for i := 0; i < 5; i++ {
+		DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/discordbot"), &gorm.Config{})
+		if err != nil {
+			log.Println("INFO: Could not connect with the database, retrying in 2 seconds")
+			time.Sleep(time.Second * 2)
+		}
+	}
 
-	DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/discordbot"), &gorm.Config{})
 	if err != nil {
 		log.Fatalln("ERR: Could not connect with the database!")
 	}
