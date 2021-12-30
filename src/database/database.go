fc3358e573312b81bd0c5374d86493e22bd91d16
Daniel Dahl <danieldahl99@gmail.com>
2021-12-30 01:40:42 +0100

    Update reconnection to database

--- src/database/database.go
+++ src/database/database.go
@@ -12,11 +12,11 @@ var DB *gorm.DB
 
 func Connect() {
 	var err error
-	for i := 0; i < 5; i++ {
+	for i := 0; i < 4; i++ {
 		DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/discordbot?parseTime=true"), &gorm.Config{})
 		if err != nil {
-			log.Println("INFO: Could not connect with the database, retrying in 2 seconds")
-			time.Sleep(time.Second * 2)
+			log.Println("INFO: Could not connect with the database, retrying in 5 seconds")
+			time.Sleep(time.Second * 5)
 		}
 	}
 
