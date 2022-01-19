48c87fbef14acc987de323b0db940f945468ae09
Dahl99 <danieldahl99@gmail.com>
2022-01-19 22:59:30 +0100

    Docker compose improvements

--- src/database/database.go
+++ src/database/database.go
@@ -13,7 +13,7 @@ var DB *gorm.DB
 func Connect() {
 	var err error
 	for i := 0; i < 4; i++ {
-		DB, err = gorm.Open(mysql.Open("root:root@tcp(db:3306)/discordbot?parseTime=true"), &gorm.Config{})
+		DB, err = gorm.Open(mysql.Open("root:root@tcp(mysql:3306)/discordbot?parseTime=true"), &gorm.Config{})
 		if err != nil {
 			log.Println("INFO: Could not connect with the database, retrying in 5 seconds")
 			time.Sleep(time.Second * 5)
